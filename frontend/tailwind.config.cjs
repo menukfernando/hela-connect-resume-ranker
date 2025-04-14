4b97162e0cc3dbf57fe89345b06106f7f6a3c2b2
menukfernando <100038759+menukfernando@users.noreply.github.com>
2025-04-14 11:20:05 +0530

    refactor

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}', // ✅ Ensures Tailwind scans all React components
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
